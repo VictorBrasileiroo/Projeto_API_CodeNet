7fa2d77c5e2755658bd785cb528fa0b1e7658d45
Your Name <victor.lopesvfk11@gmail.com>
2025-04-17 10:42:27 -0300

    Adicionar repositório de usuários e ajustes no projeto
    Implementa o `IUserRepository` e `UserRepository` no `Program.cs`, permitindo a injeção de dependência. Removidos elementos desnecessários do `CodeNet.Infrastructure.csproj` para simplificar a estrutura do projeto. Criado o arquivo `UserRepository.cs` com métodos assíncronos para gerenciar usuários no banco de dados.

--- CodeNet.Api/Program.cs
+++ CodeNet.Api/Program.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
+using CodeNet.Core.IRepositories;
 using CodeNet.Infrastructure.Context;
+using CodeNet.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +22,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
---
+++ CodeNet.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeNet.Core.IRepositories;
+using CodeNet.Core.Models;
+using CodeNet.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeNet.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context) => _context = context;
+
+        public async Task<UserModel> CreateUser(UserModel user)
+        {
+           _context.Add(user);
+           await _context.SaveChangesAsync();
+           return user;
+        }
+
+        public async Task<UserModel> DeleteUser(UserModel user)
+        {
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> EmailExist(string email) => await _context.Usuarios.AnyAsync(u => u.Email == email);
+
+        public async Task<List<UserModel>> GetAll() => await _context.Usuarios.ToListAsync();
+
+        public async Task<UserModel> GetByEmail(string email) => await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+
+        public async Task<UserModel> GetById(Guid id) => await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+
+        public async Task<UserModel> UpdateUser(UserModel user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
