1425315083f08cf5e46d3044eb9ca674df02863e
Your Name <victor.lopesvfk11@gmail.com>
2025-05-05 21:01:27 -0300

    Adiciona testes unitários e pacotes ao projeto
    Foram adicionadas referências a pacotes no arquivo `CodeNet.Api.csproj`, como `FluentAssertions`, `Moq` e `Swashbuckle.AspNetCore`, para melhorar a funcionalidade e os testes da aplicação.
    
    Foi criada a estrutura do projeto `CodeNet.UnitTests.csproj`, incluindo configurações para o framework de teste `xunit` e `coverlet.collector`, além de referências a outros projetos da solução.
    
    Um novo arquivo de teste `GrupoServiceTest.cs` foi adicionado, contendo testes unitários para a classe `GrupoService`, utilizando a biblioteca `Moq` para simular dependências e verificar o funcionamento do método `ExcluirGrupo`.

---
+++ CodeNet.UnitTests/Tests/Services/GrupoServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeNet.Application.Services.Grupo;
+using CodeNet.Core.IRepositories;
+using CodeNet.Core.Models;
+using Moq;
+using Xunit;
+
+namespace CodeNet.UnitTests.Tests.Services
+{
+    public class GrupoServiceTest
+    {
+        private readonly Mock<IGrupoMembroRepository> _mockGrupoMembroRepository;
+        private readonly Mock<IGrupoRepository> _mockGrupoRepository;
+        private readonly GrupoService _grupoService;
+
+        public GrupoServiceTest()
+        {
+            _mockGrupoMembroRepository = new Mock<IGrupoMembroRepository>();
+            _mockGrupoRepository = new Mock<IGrupoRepository>();
+            _grupoService = new GrupoService(_mockGrupoMembroRepository.Object, _mockGrupoRepository.Object);
+        }
+
+        [Fact]
+        public async Task ExcluirGrupo_GrupoExistenteEUsuarioAdmin_DeveExcluirGrupoComSucesso()
+        {
+            // Arrange
+            var grupoId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var grupo = new GrupoModel { Id = grupoId };
+            var membro = new GrupoMembroModel { IdUser = userId, Papel = "Admin" };
+
+            _mockGrupoMembroRepository.Setup(r => r.GetMembro(userId, grupoId)).ReturnsAsync(membro);
+            _mockGrupoRepository.Setup(r => r.GetById(grupoId)).ReturnsAsync(grupo);
+            _mockGrupoRepository.Setup(r => r.DeleteGrupo(grupo)).ReturnsAsync(grupo);
+
+            // Act
+            var result = await _grupoService.ExcluirGrupo(grupoId, userId);
+
+            // Assert
+            Assert.Equal(grupoId, result.Id); 
+            _mockGrupoRepository.Verify(r => r.DeleteGrupo(grupo), Times.Once);
+        }
+    }
+}
